24c9fd64fc2e5ff84c0aed0aa5495362af14c16e
David King Roderos <iamdavidkingroderos@gmail.com>
2023-09-10 10:24:11 -0700

    Added one way and round trip booking

--- RailwayReservationSystem/RailwayReservationSystem/MVVM/ViewModel/BookingViewModel.cs
+++ RailwayReservationSystem/RailwayReservationSystem/MVVM/ViewModel/BookingViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RailwayReservationSystem.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,39 @@ using System.Threading.Tasks;
 
 namespace RailwayReservationSystem.MVVM.ViewModel
 {
-    class BookingViewModel
+    class BookingViewModel : ObservableObject
     {
+        public RelayCommand OneWayBookingViewCommand { get; set; }
+        public RelayCommand RoundTripBookingViewCommand { get; set; }
+
+        public OneWayBookingViewModel OneWayBookingVM { get; set; }
+        public RoundTripBookingViewModel RoundTripBookingVM { get; set; }
+
+        private object currentView;
+
+        public object CurrentView
+        {
+            get { return currentView; }
+            set
+            {
+                currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public BookingViewModel()
+        {
+            OneWayBookingVM = new OneWayBookingViewModel();
+            RoundTripBookingVM = new RoundTripBookingViewModel();
+
+            OneWayBookingViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = OneWayBookingVM;
+            });
+            RoundTripBookingViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = RoundTripBookingVM;
+            });
+        }
     }
 }
