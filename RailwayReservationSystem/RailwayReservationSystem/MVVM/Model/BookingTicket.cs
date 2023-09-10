ce7eb3dcd39a3be8f02f64c648d76443c40441e2
David King Roderos <iamdavidkingroderos@gmail.com>
2023-09-10 16:54:22 -0700

    Added BookingTicket class (incomplete)

---
+++ RailwayReservationSystem/RailwayReservationSystem/MVVM/Model/BookingTicket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayReservationSystem.MVVM.Model
+{
+    public class BookingTicket
+    {
+        private string passengerNameRecord;
+        private string seatNumber;
+        private string origin;
+        private string destiniation;
+        private DateTime departureDate;
+
+        public string PassengerNameRecord
+        {
+            get { return this.passengerNameRecord; }
+        }
+        public string SeatNumber
+        {
+            get { return this.seatNumber; }
+        }
+
+        public BookingTicket(string passengerNameRecord, string seatNumber, string origin, string destiniation, DateTime departureDate)
+        {
+            this.passengerNameRecord = passengerNameRecord;
+            this.seatNumber = seatNumber;
+            this.origin = origin;
+            this.destiniation = destiniation;
+            this.departureDate = departureDate;
+        }
+    }
+}
