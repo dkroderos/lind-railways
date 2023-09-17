31312ea390312d0a4625d6f40fb810d790e52de2
David King Roderos <iamdavidkingroderos@gmail.com>
2023-09-17 18:05:30 +0800

    Refactor TrainSchedule class

---
+++ LINDRailways/LINDRailways/Model/TransportationEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINDRailways.Model
+{
+    public class TransportationEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageSource { get; set; }
+        public string LargeImageSource { get; set; }
+
+        public TransportationEntity(string name, string description, string imageSource, string largeImageSource)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.ImageSource = imageSource;
+            this.LargeImageSource = largeImageSource;
+        }
+    }
+}
