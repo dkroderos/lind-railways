d1608ce4fdebf7139498502abee6ede0390cc174
David King Roderos <iamdavidkingroderos@gmail.com>
2023-09-18 18:31:26 +0800

    Implemented TicketService

--- LINDRailways/LINDRailways/Model/Ticket.cs
+++ LINDRailways/LINDRailways/Model/Ticket.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +9,21 @@ namespace LINDRailways.Model
 {
     public class Ticket
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
         public string PassengerName { get; set; }
         public DateOnly DepartureDate { get; set; }
-        public TrainSchedule TrainSchedule { get; set; }
         public bool IsMale { get; set; }
         public bool IsPaid { get; set; }
+        public TrainSchedule TrainSchedule { get; set; }
 
-        public Ticket(string passengerName, DateOnly departureDate, TrainSchedule trainSchedule, bool isMale, bool isPaid)
-        {
-            this.PassengerName = passengerName;
-            this.DepartureDate = departureDate;
-            this.TrainSchedule = trainSchedule;
-            this.IsMale = isMale;
-            this.IsPaid = isPaid;
-        }
+        //public Ticket(string passengerName, DateOnly departureDate, TrainSchedule trainSchedule, bool isMale, bool isPaid)
+        //{
+        //    this.PassengerName = passengerName;
+        //    this.DepartureDate = departureDate;
+        //    this.TrainSchedule = trainSchedule;
+        //    this.IsMale = isMale;
+        //    this.IsPaid = isPaid;
+        //}
     }
 }
---
+++ LINDRailways/LINDRailways/Services/TicketService.cs
@@ -0,0 +1,58 @@
+﻿using LINDRailways.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINDRailways.Services
+{
+    public static class TicketService
+    {
+
+        private static SQLiteAsyncConnection db;
+        private static async Task Init()
+        {
+            if (db != null)
+                return;
+
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "TicketsData.db");
+
+            db = new SQLiteAsyncConnection(databasePath);
+
+            await db.CreateTableAsync<Ticket>();
+        }
+
+        public static async Task AddTicket(string passengerName, DateOnly departureDate, 
+            bool isMale, bool isPaid, TrainSchedule trainSchedule)
+        {
+            await Init();
+
+            var ticket = new Ticket
+            {
+                PassengerName = passengerName,
+                DepartureDate = departureDate,
+                IsMale = isMale,
+                IsPaid = isPaid,
+                TrainSchedule = trainSchedule
+            };
+        }
+
+        public static async Task RemoveTicket(int id)
+        {
+            await Init();
+
+            await db.DeleteAsync<Ticket>(id);
+        }
+
+        public static async Task<IEnumerable<Ticket>> GetPaidTicket()
+        {
+            await Init();
+
+            var ticket = await db.Table<Ticket>().ToListAsync();
+
+            return ticket;
+        }
+    }
+}
