af1b5a70fb3aa247ce0dc6f856de20e807ef7887
David King Roderos <iamdavidkingroderos@gmail.com>
2023-09-19 09:53:49 +0800

    Implemented deleting ticket

--- LINDRailways/LINDRailways/ViewModel/TicketDetailsViewModel.cs
+++ LINDRailways/LINDRailways/ViewModel/TicketDetailsViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using LINDRailways.Model;
+using LINDRailways.Services;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +14,37 @@ namespace LINDRailways.ViewModel
     [QueryProperty("Ticket", "Ticket")]
     public partial class TicketDetailsViewModel : BaseViewModel
     {
-        public TicketDetailsViewModel() 
-        { 
+        public TicketDetailsViewModel()
+        {
         }
 
         [ObservableProperty]
         Ticket ticket;
+
+        [RelayCommand]
+        private async Task CancelTicketAsync()
+        {
+            bool isConfirmed = await Shell.Current.CurrentPage.DisplayAlert(
+                "Cancel Ticket", "Are you sure you want to cancel this ticket?",
+                "Yes", "No");
+
+            if (!isConfirmed)
+                return;
+
+            try
+            {
+                await TicketService.RemoveTicket(Ticket.Id);
+
+                await Shell.Current.CurrentPage.DisplayAlert("Success!",
+                    "Ticket Cancelled", "OK");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await Shell.Current.CurrentPage.DisplayAlert("Error!",
+                    $"Unable to cancel ticket: {ex.Message}", "OK");
+            }
+
+        }
     }
 }
